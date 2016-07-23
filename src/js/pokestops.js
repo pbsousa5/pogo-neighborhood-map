e005de55ad4b2696ec62c9fbee9a875e60dae450
Melanie A <mkkakau@gmail.com>
2016-07-23 09:13:01 -1000

    feat: Add pokestop locations

---
+++ src/js/pokestops.js
@@ -0,0 +1,86 @@
+var pokestops = [
+  {
+    title: "Bicentennial Rock Garden",
+    position: {lat: 19.724764, lng: -155.069361}
+  },
+  {
+    title: "Alii Ice Co. Compressor",
+    position: {lat: 19.7247, lng: -155.068566}
+  },
+  {
+    title: "Historic Alii Ice Co Motor",
+    position: {lat: 19.724727, lng: -155.068315}
+  },
+  {
+    title: "Sharoan Chashitsu (Tea Room)",
+    position: {lat: 19.725722, lng: -155.067909}
+  },
+  {
+    title: "Liliuokalani Stone Lion",
+    position: {lat: 19.726166, lng: -155.067717}
+  },
+  {
+    title: "Ancient Eastern Gateway",
+    position: {lat: 19.726122, lng: -155.067491}
+  },
+  {
+    title: "Rev. Stephen Alencaster 1935 Memorial Plaque",
+    position: {lat: 19.727079, lng: -155.067678}
+  },
+  {
+    title: "Moku Ola Coconut Island",
+    position: {lat: 19.727356, lng: -155.067601}
+  },
+  {
+    title: "Liliuokalani Gardens Sign",
+    position: {lat: 19.726978, lng: -155.067352}
+  },
+  {
+    title: "Mrs Cecil B DeMille Memorial Banyan Tree",
+    position: {lat: 19.727341, lng: -155.067155}
+  },
+  {
+    title: "?",
+    position: {lat: 19.72731, lng: -155.068728}
+  },
+  {
+    title: "?",
+    position: {lat: 19.727471, lng: -155.068542}
+  },
+  {
+    title: "Wailoa River, Wailuku River",
+    position: {lat: 19.727316, lng: -155.069252}
+  },
+  {
+    title: "?",
+    position: {lat: 19.72621, lng: -155.068543}
+  },
+  {
+    title: "?",
+    position: {lat: 19.725915, lng: -155.069066}
+  },
+  {
+    title: "Ancient Eastern Gateway",
+    position: {lat: 19.726476, lng: -155.069414}
+  },
+  {
+    title: "?",
+    position: {lat: 19.725773, lng: -155.069732}
+  },
+  {
+    title: "Hilo Bay Cafe",
+    position: {lat: 19.725054, lng: -155.070222}
+  },
+  {
+    title: "?",
+    position: {lat: 19.724766, lng: -155.070659}
+  },
+  {
+    title: "Hilo Bay Pier Stone Memorial",
+    position: {lat: 19.725486, lng: -155.07118}
+  },
+  {
+    title: "Rakuen",
+    position: {lat: 19.724255, lng: -155.070375}
+  }
+];
