48229dbf1f95cc8358e99630e0af216e31a509ff
Melanie A <mkkakau@gmail.com>
2016-07-23 19:06:21 -1000

    chores: Add tasks to minify, concat, and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,55 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var htmlreplace = require('gulp-html-replace');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+var uglifyJS = require('gulp-uglify');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('clean', function () {
+  return del(['dist']);
+});
+
+gulp.task('css', ['clean'], function () {
+  return gulp.src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('all.min.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('js', ['clean'], function () {
+  return gulp.src(['src/js/pokestops.js', 'src/js/gyms.js', 'src/js/map.js'])
+    .pipe(sourcemaps.init())
+    .pipe(uglifyJS())
+    .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('html', ['clean'], function () {
+  return gulp.src('src/index.html')
+    .pipe(htmlreplace({
+      css: 'css/all.min.css',
+      js: 'js/all.min.js'
+    }))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('img', ['clean'], function () {
+  return gulp.src('src/img/*.png')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img/'));
+});
 
 gulp.task('deploy', function () {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
+
+gulp.task('default', ['clean', 'css', 'js', 'html', 'img', 'deploy']);
---
+++ src/js/map.js
@@ -0,0 +1,35 @@
+var markers = [];
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 19.725862, lng: -155.06906},
+    zoom: 17
+  });
+  setMarkers(map);
+}
+function setMarkers(map) {
+  var i = 0;
+  pokestops.forEach(function (pokestop) {
+    var pokestopMarker = new google.maps.Marker({
+      position: pokestop.position,
+      map: map,
+      title: pokestop.title,
+      icon: 'img/pokestop-marker.png',
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    i++;
+    markers.push(pokestopMarker);
+  });
+  gyms.forEach(function (gym) {
+    var gymMarker = new google.maps.Marker({
+      position: gym.position,
+      map: map,
+      title: gym.title,
+      icon: 'img/gym-marker.png',
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    i++;
+    markers.push(gymMarker);
+  });
+}
