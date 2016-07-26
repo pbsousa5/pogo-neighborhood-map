92c22774d90a69d641be0a1a80820d5c939c545b
Melanie A <mkkakau@gmail.com>
2016-07-25 14:46:34 -1000

    fix: Fix firebase errors

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,12 @@
-// Firebase setup
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD0LhmulK6Hrn-m-gfhj3XFNUq2OgfEmzg",
+  authDomain: "pogo-63931.firebaseapp.com",
+  databaseURL: "https://pogo-63931.firebaseio.com",
+};
+firebase.initializeApp(config);
+
+// Firebase variables
 var db = firebase.database();
 var pokestopsRef = db.ref('pokestops');
 var gymsRef = db.ref('gyms');
