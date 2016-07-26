048a0172ea80910a56699fa27b532ba5e3f38cc7
Melanie A <mkkakau@gmail.com>
2016-07-25 14:10:33 -1000

    feat: Add navbar and hamburger menu

--- src/js/map.js
+++ src/js/map.js
@@ -4,33 +4,32 @@ function initMap() {
     center: {lat: 19.725862, lng: -155.06906},
     zoom: 17
   });
-  console.log('In initMap: ' + pokestops.length);
-  setMarkers(map);
+  // setMarkers(map);
 }
-function setMarkers(map) {
-  var i = 0;
-  console.log('In setMarkers: ' + pokestops.length);
-  pokestops.forEach(function (pokestop) {
-    var pokestopMarker = new google.maps.Marker({
-      position: pokestop.position,
-      map: map,
-      title: pokestop.title,
-      icon: 'img/pokestop-marker.png',
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    markers.push(pokestopMarker);
-  });
-  gyms.forEach(function (gym) {
-    var gymMarker = new google.maps.Marker({
-      position: gym.position,
-      map: map,
-      title: gym.title,
-      icon: 'img/gym-marker.png',
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    i++;
-    markers.push(gymMarker);
-  });
-}
+// function setMarkers(map) {
+//   var i = 0;
+//   console.log('In setMarkers: ' + pokestops.length);
+//   pokestops.forEach(function (pokestop) {
+//     var pokestopMarker = new google.maps.Marker({
+//       position: pokestop.position,
+//       map: map,
+//       title: pokestop.title,
+//       icon: 'img/pokestop-marker.png',
+//       animation: google.maps.Animation.DROP,
+//       id: i
+//     });
+//     markers.push(pokestopMarker);
+//   });
+//   gyms.forEach(function (gym) {
+//     var gymMarker = new google.maps.Marker({
+//       position: gym.position,
+//       map: map,
+//       title: gym.title,
+//       icon: 'img/gym-marker.png',
+//       animation: google.maps.Animation.DROP,
+//       id: i
+//     });
+//     i++;
+//     markers.push(gymMarker);
+//   });
+// }
